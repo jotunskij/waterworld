0ef9628abee956c11bb7be59a2ee1f71b62effb0
Jens Tinglev <jens.tinglev@gmail.com>
2016-06-01 08:29:49 +0200

    React + foundation

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 var express = require('express');
 var basicAuth = require('basic-auth-connect');
+var bodyParser = require('body-parser');
 var app = express();
 
 app.use(basicAuth('asadani', 'Parkvillan'));
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.use(function(req, res, next) {
+  // Set permissive CORS header - this allows this server to be used only as
+  // an API server in conjunction with something like webpack-dev-server.
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Disable caching so we'll always get the latest comments.
+  res.setHeader('Cache-Control', 'no-cache');
+  next();
+});
+
+app.get('/api', function (req, res) {
+  res.json({ 'msg' : 'Hello world!'});
 });
 
 app.listen(3000, function () {
---
+++ public/react/weather.js
@@ -0,0 +1,60 @@
+/**
+ * Created by tinglev on 01/06/16.
+ */
+
+
+var WeatherDay = React.createClass({
+  render: function() {
+    return (
+      <tr>
+        <td>{this.props.validTime}</td>
+        <td>{this.props.t}</td>
+      </tr>
+      );
+  }
+});
+
+var WeatherTable = React.createClass({
+  loadWeather: function() {
+    $.ajax({
+      url: 'http://opendata-download-metfcst.smhi.se/api/category/pmp1.5g/version/1/geopoint/lat/59.318412/lon/17.670079/data.json',
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error('Error while loading weather from smhi');
+      }.bind(this)
+    })
+  },
+
+  getInitialState: function() {
+    return {data: {}};
+  },
+
+  componentDidMount: function() {
+    this.loadWeather();
+  },
+
+  render: function() {
+    var weatherDays = this.props.data.map(function(weather){
+      return (
+        <WeatherDay data={weather} />
+      );
+    });
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Day</th>
+            <th>Temp</th>
+          </tr>
+        </thead>
+        <tbody>
+          {weatherDays}
+        </tbody>
+      </table>
+    );
+  }
+});
