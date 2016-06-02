fdb461376ed3a221d47d160c60b0f051707b8b45
Jens Tinglev <jens.tinglev@gmail.com>
2016-06-02 08:11:18 +0000

    Added gpio reading

--- index.js
+++ index.js
@@ -8,23 +8,24 @@ var CronJob = require('cron').CronJob;
 // GPIO stuff
 const M_PIN = 7;
 var hertz = 0;
+var buf = new Buffer(100000);
+rpio.open(M_PIN, rpio.OUTPUT);
 
-/*gpio.setup(M_PIN, gpio.DIR_IN, gpio.EDGE_BOTH);
-
-function readValue(onDone) {
-  hertz = 0;
-  gpio.on('change', valueIncrementor);
-  setTimeout(function() {
-    gpio.removeEventListener('change', valueIncrementor);
-    onDone();
-  }, 1000);
-}
-
-function valueIncrementor(channel, value) {
-  if (value) {
-    hertz++;
+function readValue() {
+  var d1 = new Date();
+  var n1 = d1.getTime();
+  rpio.readbuf(M_PIN, buf);
+  var d2 = new Date();
+  var n2 = d2.getTime();
+  var execMs = n2 - n1;
+  var tmp = new Array(buf.length); /* 0.8 compat */
+  for (i = 0; i < buf.length; i++) {
+    tmp[i] = buf[i];
   }
-}*/
+  var str = tmp.join('').replace(/0+/g, '0').replace(/1+/g, '1');
+  var edges = (str.match(/1/g) || []).length;
+  return (edges / (execMs / 1000));  
+}
 
 // Express stuff
 var app = express();
@@ -41,10 +42,7 @@ app.use(function(req, res, next) {
 
 // API routes
 app.get('/measurement/current', function (req, res) {
-  res.json({'value' : 1});
-/*  readValue(function() {
-    res.json({ 'value' : hertz });
-  });*/
+  res.json({'value' : readValue()});
 });
 
 // Server startup
