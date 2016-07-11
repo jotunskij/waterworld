53bc10a5f30a57df08152c56e17d3a5a5a341028
Jens Tinglev <jens.tinglev@gmail.com>
2016-07-11 09:20:08 +0200

    Removed unused files

--- test.py
+++
@@ -1,8 +0,0 @@
-from __future__ import print_function
-import RPi.GPIO as GPIO
-
-GPIO.setmode(GPIO.BOARD)
-GPIO.setwarnings(False)
-GPIO.setup(11, GPIO.IN)
-while True:
-	print (GPIO.input(11), end="")
